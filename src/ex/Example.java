a4431594eb5e1beaf560e8348428d6f2459f2b2a
juni1015 <jiyoon6293@naver.com>
2023-03-14 10:23:07 +0900

    구구단

---
+++ src/ex/Example.java
@@ -0,0 +1,17 @@
+package ex;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		for(int num = 2; num < 10; num++) {
+			for(int i = 1; i < 10; i++) {
+				System.out.println(num + " * " + i + " = " + (num*i));
+			}	
+		}
+
+	}
+
+}
